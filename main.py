570ea308cc1a28449482afb3c5b81b69c9dfa8c0
AnicaP <popovicanica99@gmail.com>
2022-12-05 16:28:33 +0100

    registracija

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, session, render_template
 app = Flask(__name__)
 
+korisnici = []
+
+
 @app.route("/")
 def start():
     return render_template("Registracija.html")
@@ -9,6 +12,43 @@ def start():
 @app.route("/Login", methods=['post', 'get'])
 def login():
      return render_template('Login.html')
+
+
+
+@app.route("/Registracija", methods=['GET', 'POST'])
+def registracija():
+    global korisnici
+
+    ime = request.form['inputIme']
+    prezime = request.form['inputPrezime']
+    adresa = request.form['inputAdresa']
+    grad = request.form['inputGrad']
+    drzava = request.form['inputDrzava']
+    brojTelefona = request.form['inputBrojTelefona']
+    email = request.form['inputEmail']
+    lozinka = request.form['inputLozinka']
+
+    for k in korisnici:
+        if k["email"] == email:
+            poruka = "Korisnik sa datim email-om vec posotji!"
+            return render_template("Registracija.html", errormsg = poruka)
+
+
+    k = {
+        "ime" : ime,
+        "prezime" : prezime,
+        "adresa" : adresa,
+        "grad" : grad,
+        "drzava" : drzava,
+        "brojTelefona" : brojTelefona,
+        "email" : email,
+        "lozinka" : lozinka
+    }
+
+    korisnici.append(k)
+    return render_template("Login.html")
+
+
 
 
 if __name__ == '__main__':
