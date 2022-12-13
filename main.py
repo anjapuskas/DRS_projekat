764ff3658798a5df06c39c13792972eadd49313d
anjapuskas <anjapuskas@hotmail.com>
2022-12-13 18:16:59 +0100

    kartica

--- main.py
+++ main.py
@@ -15,16 +15,25 @@ app.secret_key = "123"
 @app.route("/")
 def start():
     return render_template("Login.html")
-
+@app.route('/Logout')
+def logout():
+    session.pop("korisnik", None)
+    return render_template("Login.html")
 
 @app.route("/PrikaziRegistraciju", methods=['POST', 'GET'])
 def prikaziRegistraciju():
      return render_template('Registracija.html')
 
+@app.route("/PrikaziDepozit", methods=['POST', 'GET'])
+def prikaziDepozit():
+     return render_template('Depozit.html')
+
 
 @app.route("/PrikaziIzmenuProfila", methods=['POST', 'GET'])
 def prikaziIzmenuProfila():
-     return render_template('IzmeniProfil.html')
+    email = session["email"]
+    korisnik = getKorisnik(email)
+    return render_template('IzmeniProfil.html', korisnik = korisnik)
 
 
 @app.route("/Registracija", methods=['GET', 'POST'])
@@ -39,9 +48,12 @@ def registracija():
     email = request.form['inputEmail']
     lozinka = request.form['inputLozinka']
     verifikovan = 0
+    stanjeNaRacunu = 2
+    stanjeUBanci = 100000
+
 
     cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO korisnik (ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)''',(ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan))
+    cursor.execute(''' INSERT INTO korisnik (ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan, stanjeNaRacunu, stanjeUBanci) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',(ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan, stanjeNaRacunu, stanjeUBanci))
     mysql.connection.commit()
     cursor.close()
 
@@ -58,22 +70,15 @@ def izmeniProfil():
     drzava = request.form['inputDrzava']
     brojTelefona = request.form['inputBrojTelefona']
     lozinka = request.form['inputLozinka']
-    email = request.form['inputEmail']
 
+    email = session["email"]
 
-    email1 = session["email"]
+    korisnik = getKorisnik(email)
 
-    if email == email1:
-        izmeniKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka)
-        poruka = "Uspesna modifikacija"
-        return render_template("Index.html", errormsg = poruka)
-    else:
-        poruka = "NIJE DOBRO"
-        return render_template("Login.html", errormsg=poruka)
-
-
-
-
+    izmeniKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka)
+    korisnik1 = getKorisnik(email)
+    poruka = "Uspesna modifikacija"
+    return render_template("Index.html", errormsg = poruka, korisnik = korisnik1)
 
 
 @app.route("/Login", methods=['GET', 'POST'])
@@ -89,7 +94,7 @@ def login():
         if korisnik["verifikovan"] == 0:
             return render_template("Verifikacija.html")
         else:
-            return render_template("Index.html", korisnik = '')
+            return render_template("Index.html",korisnik = korisnik)
     else:
         poruka = "Nespravan email ili lozinka!"
     return render_template("Login.html", errormsg=poruka)
@@ -99,19 +104,64 @@ def login():
 def verifikacija():
     ime = request.form['inputIme']
     brojKartice = request.form['inputBrojKartice']
-    datum = request.form['inputDatum']
+    mjesec = request.form['inputMjesec']
+    godina = request.form['inputGodina']
     kod = request.form['inputKod']
+
+    datumIsteka = mjesec + '/' + godina
+
+
     email = session["email"]
 
     korisnik = getKorisnik(email)
+    kartica = getKartica(brojKartice)
+    stanjeNaRacunu = korisnik["stanjeNaRacunu"]
 
-    if korisnik != None and korisnik["ime"] == ime and brojKartice == "4242424242424242" and datum == "02/23" and kod == "123":
+    if korisnik != None and korisnik["ime"] == ime and len(kod) == 3 and len(brojKartice) == 16:
         #poruka = "Uspesno logovanje!"
         verifikujKorisnika(email)
-        return render_template("Index.html")
+        povezivanjeKarticeiKorisnika(email, ime, brojKartice, datumIsteka, kod, stanjeNaRacunu)
+        korisnik1 = getKorisnik(email)
+
+        return render_template("Index.html", korisnik = korisnik1)
     else:
         poruka = "Neuspesno logovanje"
         return render_template("Verifikacija.html", errormsg=poruka)
+
+@app.route("/Depozit", methods=['GET', 'POST'])
+def uplataNaRacun():
+    kod = request.form['inputKod']
+    suma = request.form['inputSuma']
+
+    email = session["email"]
+    korisnik = getKorisnik(email)
+    kartica = getKartica(korisnik["brojKartice"])
+
+    if float(suma) <= 0:
+        poruka = "Uplata na racun mora biti  veca od 0"
+        return render_template("/Depozit.html", errormsg=poruka)
+
+    if kod != kartica["kod"]:
+        poruka = "Pogresan kod"
+        return render_template("/Depozit.html", errormsg=poruka)
+
+
+    stanjeNaRacunu = korisnik["stanjeNaRacunu"]
+    stanjeUBanci = korisnik["stanjeUBanci"]
+
+
+    if stanjeUBanci >= int(suma):
+        stanjeNaRacunu += int(suma)
+        stanjeUBanci -= int(suma)
+
+        IzmeniStanja(email, stanjeNaRacunu, stanjeUBanci)
+       # updateKorisnika(email, korisnik["ime"], korisnik["prezime"], korisnik["adresa"], korisnik["grad"], korisnik["drzava"], korisnik["brojTelefona"], korisnik["lozinka"], stanjeNaRacunu, stanjeUBanci)
+        korisnik1 = getKorisnik(email)
+        return  render_template("/Index.html", korisnik = korisnik1)
+    else:
+        poruka = "Nemate dovoljno novca na bankovnom racunu"
+        return render_template("/Depozit.html", errormsg=poruka)
+
 
 
 def getKorisnik(email: str) -> dict:
@@ -120,6 +170,27 @@ def getKorisnik(email: str) -> dict:
     korisnik = cursor.fetchone()
     cursor.close()
     return korisnik
+
+def getKartica(brojKartice: str) -> dict:
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM kartica WHERE brojKartice = %s", (brojKartice,))
+    kartica = cursor.fetchone()
+    cursor.close()
+    return kartica
+
+def povezivanjeKarticeiKorisnika(email : str, ime : str, brojKartice : str, datumIsteka : str, kod : str, stanjeNaRacunu):
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO kartica (ime, brojKartice, datumIsteka, kod) VALUES ( %s, %s, %s ,%s)''', (ime, brojKartice, datumIsteka, kod,))
+    cursor.execute(''' UPDATE korisnik SET brojKartice = %s, verifikovan = 1, stanjeNaRacunu = stanjeNaRacunu - 1 WHERE email = %s''', (brojKartice, email,))
+    mysql.connection.commit()
+    cursor.close()
+
+def IzmeniStanja(email: str, stanjeNaRacunu : int, stanjeUBanci : int):
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' UPDATE korisnik SET stanjeNaRacunu = %s, stanjeUBanci = %s WHERE email = %s ''', (stanjeNaRacunu,stanjeUBanci,email,))
+    mysql.connection.commit()
+    cursor.close()
+
 
 def verifikujKorisnika(email):
     cursor = mysql.connection.cursor()
@@ -130,9 +201,18 @@ def verifikujKorisnika(email):
 
 def izmeniKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka):
     cursor = mysql.connection.cursor()
-    cursor.execute("UPDATE korisnik SET ime = %s, prezime = %s, adresa = %s, grad = %s, drzava = %s, brojTelefona = %s, lozinka = %s WHERE email = %s", (ime, prezime, adresa, grad, drzava, brojTelefona, lozinka, email))
+    cursor.execute("UPDATE korisnik SET ime = %s, prezime = %s, adresa = %s, grad = %s, drzava = %s, brojTelefona = %s, lozinka = %s WHERE email = %s", (ime, prezime, adresa, grad, drzava, brojTelefona, lozinka, email,))
     mysql.connection.commit()
     cursor.close()
+
+
+
+def updateKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka, stanjeNaRacunu, stanjeUBanci):
+    cursor = mysql.connection.cursor()
+    cursor.execute("UPDATE korisnik SET ime = %s, prezime = %s, adresa = %s, grad = %s, drzava = %s, brojTelefona = %s, lozinka = %s, stanjeNaRacunu = %s, stanjeUBanci = %s WHERE email = %s", (ime, prezime, adresa, grad, drzava, brojTelefona, lozinka, stanjeNaRacunu, stanjeUBanci, email,))
+    mysql.connection.commit()
+    cursor.close()
+
 
 
 if __name__ == '__main__':
