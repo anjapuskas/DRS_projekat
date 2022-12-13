acfbaa877002fd284033f83e4f40c52c8640468f
AnicaP <popovicanica99@gmail.com>
2022-12-13 17:30:56 +0100

    mod

--- main.py
+++ main.py
@@ -21,9 +21,14 @@ def start():
 def prikaziRegistraciju():
      return render_template('Registracija.html')
 
+
+@app.route("/PrikaziIzmenuProfila", methods=['POST', 'GET'])
+def prikaziIzmenuProfila():
+     return render_template('IzmeniProfil.html')
+
+
 @app.route("/Registracija", methods=['GET', 'POST'])
 def registracija():
-    global korisnici
 
     ime = request.form['inputIme']
     prezime = request.form['inputPrezime']
@@ -33,14 +38,42 @@ def registracija():
     brojTelefona = request.form['inputBrojTelefona']
     email = request.form['inputEmail']
     lozinka = request.form['inputLozinka']
-
+    verifikovan = 0
 
     cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO korisnik (ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)''',(ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka))
+    cursor.execute(''' INSERT INTO korisnik (ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)''',(ime, prezime, adresa, grad, drzava, brojTelefona, email, lozinka, verifikovan))
     mysql.connection.commit()
     cursor.close()
 
     return render_template("Login.html")
+
+
+@app.route("/IzmeniProfil", methods=['GET', 'POST'])
+def izmeniProfil():
+
+    ime = request.form['inputIme']
+    prezime = request.form['inputPrezime']
+    adresa = request.form['inputAdresa']
+    grad = request.form['inputGrad']
+    drzava = request.form['inputDrzava']
+    brojTelefona = request.form['inputBrojTelefona']
+    lozinka = request.form['inputLozinka']
+    email = request.form['inputEmail']
+
+
+    email1 = session["email"]
+
+    if email == email1:
+        izmeniKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka)
+        poruka = "Uspesna modifikacija"
+        return render_template("Index.html", errormsg = poruka)
+    else:
+        poruka = "NIJE DOBRO"
+        return render_template("Login.html", errormsg=poruka)
+
+
+
+
 
 
 @app.route("/Login", methods=['GET', 'POST'])
@@ -48,12 +81,15 @@ def login():
     email = request.form['inputEmail']
     lozinka = request.form['inputLozinka']
 
-    user = getUser(email)
+    korisnik = getKorisnik(email)
 
-    if user != None and user["email"] == email and user["lozinka"] == lozinka:
+    if korisnik != None and korisnik["email"] == email and korisnik["lozinka"] == lozinka:
         #poruka = "Uspjesno logovanje!"
         session["email"] = email
-        return render_template("Verifikacija.html")
+        if korisnik["verifikovan"] == 0:
+            return render_template("Verifikacija.html")
+        else:
+            return render_template("Index.html", korisnik = '')
     else:
         poruka = "Nespravan email ili lozinka!"
     return render_template("Login.html", errormsg=poruka)
@@ -67,27 +103,36 @@ def verifikacija():
     kod = request.form['inputKod']
     email = session["email"]
 
-    user = getUser(email)
+    korisnik = getKorisnik(email)
 
-    print(email)
-    print(user != None)
-
-    if user != None and user["ime"] == ime and brojKartice == "4242424242424242" and datum == "02/23" and kod == "123":
-        #user["verifikovan"] = 1
-        poruka = "Uspesno logovanje!"
-        return render_template("Verifikacija.html", errormsg=poruka)
+    if korisnik != None and korisnik["ime"] == ime and brojKartice == "4242424242424242" and datum == "02/23" and kod == "123":
+        #poruka = "Uspesno logovanje!"
+        verifikujKorisnika(email)
+        return render_template("Index.html")
     else:
         poruka = "Neuspesno logovanje"
         return render_template("Verifikacija.html", errormsg=poruka)
 
 
-def getUser(email: str) -> dict:
+def getKorisnik(email: str) -> dict:
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM korisnik WHERE email = %s", (email,))
-    user = cursor.fetchone()
+    korisnik = cursor.fetchone()
     cursor.close()
-    return user
+    return korisnik
 
+def verifikujKorisnika(email):
+    cursor = mysql.connection.cursor()
+    cursor.execute("UPDATE korisnik SET verifikovan = 1 WHERE email = %s", (email,))
+    mysql.connection.commit()
+    cursor.close()
+
+
+def izmeniKorisnika(email, ime, prezime, adresa, grad, drzava, brojTelefona, lozinka):
+    cursor = mysql.connection.cursor()
+    cursor.execute("UPDATE korisnik SET ime = %s, prezime = %s, adresa = %s, grad = %s, drzava = %s, brojTelefona = %s, lozinka = %s WHERE email = %s", (ime, prezime, adresa, grad, drzava, brojTelefona, lozinka, email))
+    mysql.connection.commit()
+    cursor.close()
 
 
 if __name__ == '__main__':
